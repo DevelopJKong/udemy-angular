dd48a045bf72a52d5ff1b6bb76e6a6e3248f1c14
cafe small house <ybaram40@gmail.com>
2022-12-05 09:20:05 +0900

    35강 [옵션] 과제 솔루션 시청 완료

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,10 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 
 import {FormsModule} from "@angular/forms";
-import {ServerComponent} from './server/server.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
   ],
   imports: [
     BrowserModule,
